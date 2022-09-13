35dc7c6c2665413d274edb9c6d145b90ca374098
Scripted <cb443151@gmail.com>
2022-09-13 20:03:02 +0000

    Pending changes exported from your codespace

--- bot.lua
+++ bot.lua
@@ -1,2 +1,9 @@
 local discordia = require('discordia')
 local client = discordia.Client()
+
+client:on('ready', function()
+	-- client.user is the path for your bot
+	print('Logged in as '.. client.user.username)
+end)
+
+client:run('Bot')
