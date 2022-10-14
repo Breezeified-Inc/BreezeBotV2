0af3d72ad18f3b61329623b9cfe46279df93b0d1
Scripted <cb443151@gmail.com>
2022-10-14 16:48:14 +0000

    working on basics

--- bot.lua
+++ bot.lua
@@ -1,9 +1,34 @@
 local discordia = require('discordia')
 local client = discordia.Client()
+local dslash = require("discordia-slash")
+-- required to initialize:
+local CLIENT = discordia.Client():useApplicationCommands()
+local fs = require('fs')
+local json = require('json')
+
+local config = json.decode(fs.readFileSync('config.json'))
+
 
 client:on('ready', function()
 	-- client.user is the path for your bot
 	print('Logged in as '.. client.user.username)
 end)
 
-client:run('Bot')
+CLIENT:on("messageCommand", function(ia, cmd, msg)
+	CommandConstructor = CommandConstructor:setName(name)
+	CommandConstructor = CommandConstructor:setDescription(description)
+end)
+
+
+
+client:on('messageCreate', function(message)
+	if message.content == '!ping' then
+		
+		message.channel:send('Latency is' .. os.Time() - message.createdAt )
+	end
+end)
+
+
+
+
+client:run(config.token)
